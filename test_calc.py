9b22dcf111cccc636377d404e302964f1c22f527
Steven Nguyen <steven.nguyen89@gmail.com>
2019-08-25 10:28:47 -0400

    Add add and subtract methods + test cases

---
+++ test_calc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import unittest
+import simple_calc
+
+class SimpleCalcTest(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(simple_calc.add(5, 3), 8)
+        self.assertEqual(simple_calc.add(5, -3), 2)
+        self.assertEqual(simple_calc.add(-5, -3), -8)
+
+    def test_subtract(self):
+        self.assertEqual(simple_calc.subtract(5, 3), 2)
+        self.assertEqual(simple_calc.subtract(5, -3), 8)
+        self.assertEqual(simple_calc.subtract(-5, -3), -2)
+
+if __name__ == '__main__':
+    unittest.main()
