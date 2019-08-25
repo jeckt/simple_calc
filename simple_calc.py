327ef9f5ccc667d735812778ad6a3bd923769c7d
Steven Nguyen <steven.nguyen89@gmail.com>
2019-08-25 11:00:23 -0400

    Add divide feature with test cases

--- simple_calc.py
+++ simple_calc.py
@@ -3,3 +3,11 @@ def add(x, y):
 
 def subtract(x, y):
     return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Division by zero")
+    return x / y
--- test_calc.py
+++ test_calc.py
@@ -16,9 +16,31 @@ class SimpleCalcTest(unittest.TestCase):
         self.assertEqual(simple_calc.subtract(-5, -3), -2)
 
     def test_multiply(self):
-        self.assertEqual(simple_calc.multiply(5, 3), 2)
-        self.assertEqual(simple_calc.multiply(5, -3), 8)
-        self.assertEqual(simple_calc.multiply(-5, -3), -2)
+        self.assertEqual(simple_calc.multiply(5, 3), 15)
+        self.assertEqual(simple_calc.multiply(5, -3), -15)
+        self.assertEqual(simple_calc.multiply(-5, -3), 15)
+
+    def test_integer_divide(self):
+        self.assertEqual(simple_calc.divide(10, 2), 5)
+        self.assertEqual(simple_calc.divide(-12, 3), -4)
+        self.assertEqual(simple_calc.divide(12, -4), -3)
+        self.assertEqual(simple_calc.divide(-12, -4), 3)
+
+    def test_float_divide(self):
+        self.assertEqual(simple_calc.divide(5, 2), 2.5)
+        self.assertEqual(simple_calc.divide(-5, 2), -2.5)
+        self.assertEqual(simple_calc.divide(5, -2), -2.5)
+        self.assertEqual(simple_calc.divide(-5, -2), 2.5)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            simple_calc.divide(0, 0)
+
+        with self.assertRaises(ValueError):
+            simple_calc.divide(5, -0)
+
+        with self.assertRaises(ValueError):
+            simple_calc.divide(7, -0.0)
 
 if __name__ == '__main__':
     unittest.main()
